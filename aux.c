6770aeae6000f0727167437d69dfaef91615ea60
JoaoHL <jao.luciano8@gmail.com>
2017-08-21 18:46:06 -0300

    adicionados pseudo-esqueletos do bash

---
+++ aux.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include "aux.h"
+
+char* prompt_terminal() {
+	return NULL;
+}
+
+void open_process(char* process) {
+	return;
+}
+
+void exec_command(char* command) {
+	return;
+}
---
+++ aux.h
@@ -0,0 +1,9 @@
+#ifndef AUX_HEADER
+#define AUX_HEADER
+
+char* prompt_terminal();
+void open_process(char* process);
+void exec_command(char* command);
+
+
+#endif
