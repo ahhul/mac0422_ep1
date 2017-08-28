10ffb29c886bb6b523a987618c6fd62f52aa7352
JoaoHL <jao.luciano8@gmail.com>
2017-08-28 20:26:29 -0300

    comando 'date' implementado e rodando liso

--- main.c
+++ main.c
@@ -1,17 +1,20 @@
-#include "aux.h"
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
 #include <string.h>
+#include <time.h>
+#include <sys/time.h>
+#include <sys/syscall.h>
 #include <sys/wait.h>
 #include <sys/types.h>
 
+#include "aux.h"
+
 int main(int argc, char* argv[]) {
+	struct timeval *crude_time =  mallocc(sizeof(struct timeval));
 	pid_t child_pid;
-	int err;
-	int status;
-	char* command = NULL;
-	char* gid;
+	int err, status;
+	char *gname, *date_str, *command = NULL;
 	char** parsed_command = NULL;
 
 	while(1) {
@@ -25,24 +28,38 @@ int main(int argc, char* argv[]) {
 		if (strcmp(parsed_command[0], "exit") == 0) {
 			free(command);
 			free(parsed_command);
-			exit(0);
+			free(crude_time);
+			break;
 		}
 
 		/* comando "chown :<gid> <nome do arquivo>" */
 		else if (strcmp(parsed_command[0], "chown") == 0) {
 			/* tira o ":" do group ID */
-			gid = strtok(parsed_command[1], ":");
-			err = chown(parsed_command[3], -1, atoi(gid));
+			gname = strtok(parsed_command[1], ":");
+			err = chown(parsed_command[3], -1, atoi(gname));
 
 			if (err == -1) {
-				printf("chown deu ruim\n");
+				printf("chown: Operação cancelada.\n");
 			}
 			free(command);
 			free(parsed_command);
 		}
 
 		else if (strcmp(parsed_command[0], "date") == 0) {
+			date_str = mallocc(50 * sizeof(char));
 
+			/* chamada de sistema gettimeofday() para pegar o tempo do sistema*/
+			gettimeofday(crude_time, NULL);
+
+			/* Formata os dados da estrutura "crude_time" do jeito que queremos,
+			usando o campo "tv_sec" e formatando para o horario local */
+			strftime(date_str, 50, "%a %b %d %X %Z %Y\n", localtime(&(crude_time->tv_sec)));
+
+			printf("%s", date_str);
+
+			free(command);
+			free(parsed_command);
+			free(date_str);
 		}
 
 		/* código do processo filho */
